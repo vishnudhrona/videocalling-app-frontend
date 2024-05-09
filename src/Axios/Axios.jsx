61c023ae1d9d4251905014e12bb75b957888a493
Vishnu v <vishnuvaliyaveetil92@gmail.com>
2024-05-09 11:43:15 +0530

    finished

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://videocalling-app-backend-1.onrender.com',
     // timeout : 15000,
     headers:{
         "Content-Type": "application/json"
